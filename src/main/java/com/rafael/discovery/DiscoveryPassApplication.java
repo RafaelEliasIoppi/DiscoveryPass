7fa9ce53431e67869cd4da518ec65ac2cefd6719
Rafael Elias Ioppi <141973836+RafaelEliasIoppi@users.noreply.github.com>
2026-03-10 11:13:52 +0000

    rafael

---
+++ src/main/java/com/rafael/discovery/DiscoveryPassApplication.java
@@ -0,0 +1,13 @@
+package com.rafael.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryPassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryPassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafael/discovery/DiscoveryPassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafael.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryPassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
